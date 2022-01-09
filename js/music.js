78457d5f5c82e4b151d9b9254518e4e89865e414
Josh Devine <joshuaddevine@gmail.com>
2022-01-09 12:39:22 -0500

    Moved extra style additions into single for switch block

--- js/music.js
+++ js/music.js
@@ -24,15 +24,12 @@ function generateEntries(){
         let rank = document.createElement("h2");
         rank.innerHTML = entry["rank"];
         rank.classList.add("number")
-        if ("number" in styles) number.classList.add(styles["number"]);
 
         let cover = document.createElement("img");
         cover.src = IMG_URL_BASE + (songs.length - i) + ".jpg";
-        if ("cover" in styles) cover.classList.add(styles["cover"]);
 
         let header = document.createElement("h2");
         header.innerHTML = entry["artist"] + ": \"" + entry["title"] + "\" (" + entry["year"] + ")";
-        if ("header" in styles) header.classList.add(styles["header"]);
 
         let write_up = document.createElement("div");
         if (entry["write_up"] != ""){
@@ -40,9 +37,23 @@ function generateEntries(){
         } else {
             write_up.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida neque sed metus bibendum sagittis. Sed tempus metus ut arcu scelerisque, vitae suscipit mauris suscipit. Morbi eu sapien vel purus blandit vulputate. Sed a ante bibendum, rutrum erat et, tincidunt velit. Donec varius felis magna, sit amet elementum mi consectetur vel. Morbi non ligula ac massa pretium lacinia ac ut felis. Aenean egestas suscipit mi in fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida neque sed metus bibendum sagittis. Sed tempus metus ut arcu scelerisque, vitae suscipit mauris suscipit. Morbi eu sapien vel purus blandit vulputate. Sed a ante bibendum, rutrum erat et, tincidunt velit. Donec varius felis magna, sit amet elementum mi consectetur vel. Morbi non ligula ac massa pretium lacinia ac ut felis."
         }
-
-        if ("write_up" in styles) write_up.classList.add(styles["write_up"]);
-
+        
+        for (let style in styles){
+            switch (style) {
+                case "number":
+                    rank.classList.add(styles[style]);
+                    break;
+                case "cover":
+                    cover.classList.add(styles[style]);
+                    break;
+                case "header":
+                    header.classList.add(styles[style]);
+                    break;
+                case "write_up":
+                    write_up.classList.add(styles[style]);
+                    break;
+            }
+        }
 
         // append entry elements to list item
         list_item.appendChild(divider);
